d5fc4fe94dd64dd57aa4d43ed91baeb50341a04c
Javier Cazorla <sherlock722@gmail.com>
2015-12-05 18:44:01 +0100

    Se implementan los protocolos Equatable y Comparable

--- HackerBooks/HackerBooks/AGTBook.swift
+++ HackerBooks/HackerBooks/AGTBook.swift
@@ -8,9 +8,13 @@
 
 import Foundation
 
-class AGTBook {
+//Implementamos el protocolo Equatable
+//(para ver si dos objetos son equivalentes). Este protocolo puede
+//servir para ordenar
+
+class AGTBook : Equatable, Comparable {
     
-    let titulo    : String
+    let titulo    : String?
     let autores   : NSArray
     let tags      : NSArray
     let imageBook : NSURL
@@ -18,7 +22,7 @@ class AGTBook {
     
 
     //MARK: - Init
-    init (titulo : String,
+    init (titulo : String?,
         autores  : NSArray,
         tags     : NSArray,
         imageBook: NSURL,
@@ -31,6 +35,26 @@ class AGTBook {
             self.pdfBook = pdfBook
             
     }
+    
+    
+    //MARK - Proxies (Para comparar por propiedades)
+    //Protocolo EQUATABLE
+    var proxyForComparision : String {
+        
+        get {
+            return "\(titulo)\(autores)\(tags)\(imageBook)\(pdfBook)"
+        }
+        
+    }
+    
+    //Protocolo COMPARABLE
+    var proxyForSorting : String{
+        
+        get{
+            return "\(titulo)\(tags)"
+        }
+    }
+    
     
     /*convenience init (autores  : NSArray,
         tags     : NSArray,
@@ -46,9 +70,79 @@ class AGTBook {
 
 }
 
-//Implementamos a través de una extensión el protocolo Equatable
-//extension AGTBook:Equatable{
+//Implementamos a traves de una extension el protocolo
+//CustomStringConvertible que es como el description en
+//ObjectiveC
+
+extension AGTBook : CustomStringConvertible{
     
+    var description : String {
+     
+        get{
+            
+            if let titulo = titulo {
+                return "<\(self.dynamicType): \(titulo)>"
+            }else{
+                return "<\(self.dynamicType)>"
+            }
+            
+        }
+        
+    }
     
+}
+
+//Si fuera una extension para implementar EQUTABLE: Quedaría
+/*extension AGTBook : Equatable{
     
-//}
+    //Estaria vacia ya que se define la función (operador) fuera
+    //de la extensión
+    
+}*/
+
+//MARK: - Operators
+
+//Protocolo EQUATABLE (==)
+func == (lhs : AGTBook, rhs : AGTBook) -> Bool{
+    
+    //Son el mismo objeto
+    guard !(lhs === rhs) else{ //Comparo direcciones de memoria
+        return true
+    }
+    
+    //Son clases distintas
+    guard lhs.dynamicType == rhs.dynamicType else {
+        return false
+    }
+    
+    //Comparar todas las propiedades (patrón de diseño del proxy)
+    return (lhs.proxyForComparision == rhs.proxyForComparision)
+
+}
+
+//EQUATABLE(<>)
+//El compilador crea  el  <> (distinto) por haber implementado el ==
+
+
+
+//Protocolo COMPARABLE
+func < (lhs: AGTBook, rhs: AGTBook) -> Bool {
+    
+    return (lhs.proxyForSorting < rhs.proxyForSorting)
+}
+
+//El resto de funciones (>) (<=) y (>=) los deduce el compilador
+
+
+
+
+
+
+
+
+
+
+
+
+
+
