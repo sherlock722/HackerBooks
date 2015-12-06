dd54db93060ef370123fc412c6a12cf0a81f43a1
Javier Cazorla <sherlock722@gmail.com>
2015-12-06 21:43:54 +0100

    Se crea los metodos de la clase AGTLibrary

--- HackerBooks/HackerBooks/AGTLibrary.swift
+++ HackerBooks/HackerBooks/AGTLibrary.swift
@@ -10,17 +10,60 @@
 import Foundation
 
 
-class AGTLibrary {
 
-    //Array de libros
-    var books : NSMutableArray
+
+class AGTLibrary {
     
+    
+    //MARK: - Private Interface
     
     //Array de tags con todas las distintas tematicas en
-    //orden alfabetico. No piede bajo ningún concepto haber
-    //ninguno repetido
-    var tags : NSMutableArray
+    //orden alfabetico. No puede bajo ningún concepto
+    //haber ninguno repetido
+    private var tags : NSMutableArray
     
+    //Se guardan los libros en un diccionario
+    //cuyas claves son los tags y su valor son
+    //arrays de AGTBook
+    private let books : [String:[AGTBook]]
+    
+    
+    
+    //MARK: - Initialization
+    init (arrayOfBooks: [AGTBook]){
+        
+        //Se inicializa la propiedad books y se le meten libros
+        var books = Dictionary<String,Array<AGTBook>>()
+    
+        /*for each in self.tags {
+            
+            books[each] = Array<AGTBook>
+            
+        }*/
+        
+        //Segun los tags lo metemos en un sitio u otro
+        
+        for book in arrayOfBooks{
+            
+            
+            books[tags]?.append(book)
+         
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    //Array de libros
+    //var books : NSMutableArray
+    
+    
+    
+    //MARK: Public interface
     
     //Numero total de libros
     var booksCount : Int {
@@ -30,19 +73,49 @@ class AGTLibrary {
             
         }
     }
-
-
-    //Cantidad de libros que hay en una temática
-    //Si el tag no existe debe devolver cero
-    func bookCountForTag (tag : String?)->Int{
-        
+    
+    //Numero Total de Tags
+    var tagsCount : Int{
+        get{
+            
+            let count : Int = self.tags.count
+            return count
+        }
     }
 
     
+    //Cantidad de libros que hay en una temática
+    //Si el tag no existe debe devolver cero
+    
+    //Repasar
+    func bookCountForTag (tag : String) -> Int{
+        
+        if let bks = books[tag]{
+            
+            return bks.count
+        }
+        
+        return 0
+    }
+    
+    //El libro n del tag m
+    subscript (idx: Int, inTag tag: String)->AGTBook?{
+        //Obtener el array de tags y el libro en esa posicion
+        get {
+            if let bks = books[tag]{
+                return bks[idx]
+            }
+            return nil
+        }
+    }
+    
+    
+    /*
+    
     //Array de libros (instancias de AGTBook) que hay en
     //una tematica
-    //Un libro puede estar en una o más temáticas. si no 
-    //libros para una temática, ha de devolver nil
+    //Un libro puede estar en una o más temáticas. si no
+    //hay libros para una temática, ha de devolver nil
     func booksForTag (tag : String?) -> [AGTBook]{
         
     }
@@ -59,7 +132,7 @@ class AGTLibrary {
         
     }
     
-    
+    */
     
 }
 
